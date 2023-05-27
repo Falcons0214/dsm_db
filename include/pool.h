f5d3d3e9dc5cfaa4dd2bc2481a29e1d4e59276f8
Falcons0214 <jeffery12366@yahoo.com>
2023-05-27 11:19:38 +0800

    Author: jojo Date:   2023/05/27 Title: Rewrite buffer pool layout
    1. Add address_index_table use to find the sub pool is block stay.
    2. Add get_spm_block & free_spm_block use two funcs manage block.
    3. Remove block_address_table because not need.
    4. Move flag & priority in block, because it should in block.

--- include/pool.h
+++ include/pool.h
@@ -23,7 +23,9 @@ typedef struct sub_pool_s sub_pool_s;
 #define PAGELOADFAIL UINT32_MAX - 1
 #define PAGESWAPACCP UINT32_MAX - 2
 #define PAGESWAPFAIL UINT32_MAX - 3
+
 #define PAGEISPIN UINT32_MAX - 4
+#define PAGECANFREE UINT32_MAX - 5
 
 // for pb_index state, use [255] maximun status: 1677215
 #define PAGENOTINPOOL UINT32_MAX
@@ -40,19 +42,10 @@ struct sub_pool_s
     block_s *buf_list;
     page_s *pages;
     
-    /*
-     * Why separate state_table & priority table from block, because locality.
-     * Page State Table: Record state about page, like: pin, dirty ...
-     * Page Prio Table: Use for schedule
-     */
     uint32_t used_blocks;
     block_s *empty_block;
 
-    block_s **block_addr_table;
-    uint8_t *page_state_table;
-    uint8_t *page_priority_table;
-
-    pthread_mutex_t *sub_pool_mutex;
+    pthread_mutex_t sub_pool_mutex;
 };
 
 struct pool_mg_s
@@ -61,34 +54,34 @@ struct pool_mg_s
     block_s *block_header;
     char *pool;
 
+    void *address_index_table[SUBPOOLS];
+
     // waiting_queue: Use for check those pages, they want be access but not in memory yet.
     list_s waiting_queue;
 };
 
-inline void separate_index(uint32_t, uint8_t*, uint32_t*);
-inline uint32_t combine_index(uint32_t, uint32_t);
 inline bool is_pool_full(pool_mg_s*);
+inline bool can_page_swap(block_s*);
+
+inline uint32_t get_block_spm_index(pool_mg_s*, block_s*);
+
+inline block_s* get_spm_block(sub_pool_s*);
+inline uint32_t free_spm_block(sub_pool_s*, block_s*);
+
 void block_priority_incr(pool_mg_s*, uint32_t);
 
 pool_mg_s* pool_open();
 void pool_close();
-bool pool_schedule(pool_mg_s*, disk_mg_s*, uint32_t);
+void pool_schedule(pool_mg_s*, disk_mg_s*, uint32_t);
 uint32_t find_useful_pool(pool_mg_s*);
-uint32_t page_swap_out(pool_mg_s*, disk_mg_s*, uint32_t);
-uint32_t page_bring_in(pool_mg_s*, disk_mg_s*, uint32_t, uint32_t);
 
-/*
- * mp_access_page_by_pid:
- *
- * First byte of return value will used for indicate, \
- * which pool is the block stay.
- * 
- * Last three bytes of return value for block index.
- */
-uint32_t mp_access_page_by_pid(pool_mg_s*, disk_mg_s*, uint32_t);
-bool mp_require_page_rlock(pool_mg_s*, uint32_t);
-void mp_release_page_rlock(pool_mg_s*, uint32_t);
-bool mp_require_page_wlock(pool_mg_s*, uint32_t);
-void mp_release_page_wlock(pool_mg_s*, uint32_t);
+uint32_t page_swap_out(disk_mg_s*, block_s*);
+uint32_t page_bring_in(disk_mg_s*, uint32_t, block_s*);
+
+block_s* mp_access_page_by_pid(pool_mg_s*, disk_mg_s*, uint32_t);
+bool mp_require_page_rlock(pool_mg_s*, block_s*);
+void mp_release_page_rlock(pool_mg_s*, block_s*);
+bool mp_require_page_wlock(pool_mg_s*, block_s*);
+void mp_release_page_wlock(pool_mg_s*, block_s*);
 
 #endif /* POOL_H */
--- src/pool/pool.c
+++ src/pool/pool.c
@@ -9,66 +9,33 @@
 
 #define FILELOCATION "pool.c"
 
-void separate_index(uint32_t src, uint8_t *p_index, uint32_t *b_index)
-{
-    memcpy(p_index, (((char*)&src) + 3), 1);
-    *b_index = src & 0x00ffffff;
-}
+// void separate_index(uint32_t src, uint8_t *p_index, uint32_t *b_index)
+// {
+//     memcpy(p_index, (((char*)&src) + 3), 1);
+//     *b_index = src & 0x00ffffff;
+// }
 
-uint32_t combine_index(uint32_t pool_index, uint32_t block_index)
-{
-    uint32_t pb_index;
-    char *index = (char*)&pb_index;
-    memcpy(index + 3, &pool_index, 1);
-    memcpy(index, &block_index, 3);
-    return pb_index;
-}
-
-uint32_t get_page_pb_index(pool_mg_s *pm, uint32_t page_id)
-{
-    uint32_t i, h, b = 0;
-    for (i=0; b == 0 && i<SUBPOOLS; i++) {
-        for (h=0; h<pm->sub_pool[i].used_blocks; h++) {
-            if (pm->sub_pool[i].block_addr_table[h]->page->page_id == page_id) {
-                b = 1;
-                break;
-            }
-        }
-    }
-    return (b) ? combine_index(i, h) : PAGENOTINPOOL;
-}
+// uint32_t combine_index(uint32_t pool_index, uint32_t block_index)
+// {
+//     uint32_t pb_index;
+//     char *index = (char*)&pb_index;
+//     memcpy(index + 3, &pool_index, 1);
+//     memcpy(index, &block_index, 3);
+//     return pb_index;
+// }
 
 bool is_pool_full(pool_mg_s *pm)
 {
     int sum = 0;
-    for (int i=0; i<SUBPOOLS; i++)
+    for (int i = 0; i < SUBPOOLS; i ++)
         sum += pm->sub_pool[i].used_blocks;
     return (sum == PAGEBLOCKS) ? true : false;
 }
 
 // uncheck, may accessed by multi thread !!
-uint32_t page_swap_out(pool_mg_s *pm, disk_mg_s *dm, uint32_t pb_index)
+uint32_t page_swap_out(disk_mg_s *dm, block_s *block)
 {
-    uint8_t p_index;
-    uint32_t b_index;
-    separate_index(pb_index, &p_index, &b_index);
-
-    block_s *block = pm->sub_pool[p_index].block_addr_table[b_index];
-    char page_state = pm->sub_pool[p_index].page_state_table[b_index];
-    
-    if (PINCHECK(page_state))
-        return PAGEISPIN;
-
-    pm->sub_pool[p_index].page_state_table[b_index] = 0;
-    pm->sub_pool[p_index].page_priority_table[b_index] = 0;
-
-    pthread_mutex_lock(pm->sub_pool[p_index].sub_pool_mutex);
-    block->next_empty = pm->sub_pool[p_index].empty_block;
-    pm->sub_pool[p_index].empty_block = block;
-    pm->sub_pool[p_index].used_blocks --;
-    pthread_mutex_unlock(pm->sub_pool[p_index].sub_pool_mutex);
-
-    if (DIRTYCHECK(page_state))
+    if (DIRTYCHECK(block->flags))
         if (dk_write_page_by_pid(dm, block->page->page_id, (void*)block->page) == DKWRITEINCOMP)
             return PAGESWAPFAIL;
     
@@ -76,21 +43,9 @@ uint32_t page_swap_out(pool_mg_s *pm, disk_mg_s *dm, uint32_t pb_index)
 }
 
 // uncheck, may accessed by multi thread !!
-uint32_t page_bring_in(pool_mg_s *pm, disk_mg_s *dm, uint32_t page_id, uint32_t pb_index)
+uint32_t page_bring_in(disk_mg_s *dm, uint32_t page_id, block_s *block)
 {
-    block_s *empty;
-    uint8_t p_index;
-    uint32_t b_index;
-    separate_index(pb_index, &p_index, &b_index);
-
-    pthread_mutex_lock(pm->sub_pool[p_index].sub_pool_mutex);
-    empty = pm->sub_pool[p_index].empty_block;
-    pm->sub_pool[p_index].empty_block = pm->sub_pool[p_index].empty_block->next_empty;
-    pm->sub_pool[p_index].used_blocks ++;
-    pm->sub_pool[p_index].block_addr_table[b_index] = empty;
-    pthread_mutex_unlock(pm->sub_pool[p_index].sub_pool_mutex);
-
-    if (dk_read_page_by_pid(dm, page_id, (void*)empty->page) == DKREADINCOMP)
+    if (dk_read_page_by_pid(dm, page_id, (void*)block->page) == DKREADINCOMP)
         return PAGELOADFAIL;
     return PAGELOADACCP;
 }
@@ -112,7 +67,7 @@ void* get_space_alignment_by(uint32_t value, uint32_t size)
         fail = 0;
         
         // check memory is actually alignment by value !
-        for (int i=0; i<bits-1; i++, tmp >>= 1) {
+        for (int i = 0; i < bits-1; i ++, tmp >>= 1) {
             if (tmp & 0x00000001) {
                 fail = 1;
                 free(buffer);
@@ -140,29 +95,18 @@ pool_mg_s* pool_open()
     sub_pool_s *temp;
 
     int cur_sub_pool = 0;
-    for (int i=0; i<PAGEBLOCKS; i++, tmp_p++, tmp_b++) {
+    for (int i = 0; i < PAGEBLOCKS; i ++, tmp_p ++, tmp_b ++) {
         if (i % SUBPOOLBLOCKS == 0) {
+
+            pool->address_index_table[cur_sub_pool] = tmp_b;
+
             temp = &pool->sub_pool[cur_sub_pool];
-
-            temp->buf_list = pool->block_header;
+            temp->buf_list = tmp_b;
             temp->empty_block = tmp_b;
-            temp->pages = (page_s*)tmp_p;
+            temp->pages = tmp_p;
             temp->used_blocks = 0;
+            pthread_mutex_init(&(pool->sub_pool[cur_sub_pool].sub_pool_mutex), NULL);
 
-            temp->block_addr_table = (block_s**)malloc(sizeof(block_s*) * SUBPOOLBLOCKS);
-            ECHECK_MALLOC(temp->block_addr_table, FILELOCATION);
-
-            temp->page_state_table = (uint8_t*)malloc(sizeof(uint8_t) * SUBPOOLBLOCKS);
-            ECHECK_MALLOC(temp->page_state_table, FILELOCATION);
-
-            temp->page_priority_table = (uint8_t*)malloc(sizeof(uint8_t) * SUBPOOLBLOCKS);
-            ECHECK_MALLOC(temp->page_priority_table, FILELOCATION);
-
-            temp->sub_pool_mutex = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
-            ECHECK_MALLOC(temp->sub_pool_mutex, FILELOCATION);
-            pthread_mutex_init(pool->sub_pool[cur_sub_pool].sub_pool_mutex, NULL);
-
-            memset(temp->page_state_table, 0, SUBPOOLBLOCKS);
             cur_sub_pool ++;
         }
         if (i % SUBPOOLBLOCKS == SUBPOOLBLOCKS - 1)
@@ -177,9 +121,64 @@ pool_mg_s* pool_open()
     return pool;
 }
 
-bool pool_schedule(pool_mg_s *pm, disk_mg_s *dm, uint32_t need_blocks)
+block_s* get_spm_block(sub_pool_s *spm)
 {
-    
+    block_s *block;
+    if (!spm->empty_block) return NULL;
+    pthread_mutex_lock(&spm->sub_pool_mutex);
+    block = spm->empty_block;
+    spm->empty_block = block->next_empty;
+    pthread_mutex_unlock(&spm->sub_pool_mutex);
+
+    OCCUPYSET(&(block->flags));
+
+    return block;
+}
+
+uint32_t free_spm_block(sub_pool_s *spm, block_s *block)
+{
+    if (PINCHECK(block->flags))
+        PAGEISPIN;
+
+    pthread_mutex_lock(&(spm->sub_pool_mutex));
+    block->next_empty = spm->empty_block;
+    spm->empty_block = block;
+    spm->used_blocks --;
+    pthread_mutex_unlock(&(spm->sub_pool_mutex));
+
+    block->flags = 0;
+    block->priority = BLOCKPRIINIT;
+    return PAGECANFREE;
+}
+
+uint32_t get_block_spm_index(pool_mg_s *pm, block_s *block)
+{
+    for (int i = 0; i < SUBPOOLS; i ++)
+        if (block > pm->address_index_table[i])
+            return i;
+}
+
+bool can_page_swap(block_s *block)
+{
+    return (PINCHECK(block->flags)) ? true : false;
+}
+
+// Find the block it can swap in disk, if not return NULL otherwise return need blocks index.
+void pool_schedule(pool_mg_s *pm, disk_mg_s *dm, uint32_t need_blocks)
+{
+    int n_index = 0;
+    uint32_t spm_index;
+    block_s *needs[need_blocks];
+
+    for (int i = 0; i < SUBPOOLS && n_index < need_blocks; i ++)
+        for (int h = 0; h < SUBPOOLBLOCKS && n_index < need_blocks; h ++)
+            if (can_page_swap(&(pm->sub_pool[i].buf_list[h])) && n_index < need_blocks)
+                needs[n_index ++] = &(pm->sub_pool[i].buf_list[h]);
+ 
+    // for (int i = 0; i < n_index; i++) {
+    //     if(page_swap_out(dm, needs[i]) == PAGESWAPFAIL) {}
+    //     spm_index = get_block_spm_index(pm, needs[i]);
+    // }
 }
 
 void block_priority_incr(pool_mg_s *pm, uint32_t pb_index)
@@ -187,67 +186,38 @@ void block_priority_incr(pool_mg_s *pm, uint32_t pb_index)
 
 }
 
-uint32_t mp_access_page_by_pid(pool_mg_s *pm, disk_mg_s *dm, uint32_t page_id)
+block_s* mp_access_page_by_pid(pool_mg_s *pm, disk_mg_s *dm, uint32_t page_id)
 {
-    uint32_t pb_index = get_page_pb_index(pm, page_id);
-
-    if (pb_index != PAGENOTINPOOL) {
-        block_priority_incr(pm, pb_index);
-        return pb_index;
-    }
-
     // check is it have different thread access the same page, if exist combine the access.
-
     // if pool is full, p_scheduler();
+
     if (is_pool_full(pm)) {
         pool_schedule(pm, dm, 1);
     }
-    
-
 
 
     
-    return pb_index;
+    return NULL;
 }
 
 
-bool mp_require_page_rlock(pool_mg_s *pm, uint32_t pb_index)
+bool mp_require_page_rlock(pool_mg_s *pm, block_s *block)
 {
-    uint8_t pool_index;
-    uint32_t block_index;
-    separate_index(pb_index, &pool_index, &block_index);
-
-    block_s *block = pm->sub_pool[pool_index].block_addr_table[block_index];
     return r_rwlock(&(block->rwlock));
 }
 
-void mp_release_page_rlock(pool_mg_s *pm, uint32_t pb_index)
+void mp_release_page_rlock(pool_mg_s *pm, block_s *block)
 {
-    uint8_t pool_index;
-    uint32_t block_index;
-    separate_index(pb_index, &pool_index, &block_index);
-
-    block_s *block = pm->sub_pool[pool_index].block_addr_table[block_index];
     r_unrwlock(&(block->rwlock));
 }
 
-bool mp_require_page_wlock(pool_mg_s *pm, uint32_t pb_index)
+bool mp_require_page_wlock(pool_mg_s *pm, block_s *block)
 {
-    uint8_t pool_index;
-    uint32_t block_index;
-    separate_index(pb_index, &pool_index, &block_index);
-
-    block_s *block = pm->sub_pool[pool_index].block_addr_table[block_index];
     return w_rwlock(&(block->rwlock));
 }
 
-void mp_release_page_wlock(pool_mg_s *pm, uint32_t pb_index)
+void mp_release_page_wlock(pool_mg_s *pm, block_s *block)
 {
-    uint8_t pool_index;
-    uint32_t block_index;
-    separate_index(pb_index, &pool_index, &block_index);
-
-    block_s *block = pm->sub_pool[pool_index].block_addr_table[block_index];
     w_unrwlock(&(block->rwlock));
 }
 
