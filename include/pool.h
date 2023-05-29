769a1e5fa6fea44ba13a555c69472a4f834a9bc9
Falcons0214 <jeffery12366@yahoo.com>
2023-05-29 10:42:56 +0800

    Author: jojo Date: 2023/05/29 Title: Delete pool.c pool.h
    It is garbage so byby.

--- include/pool.h
+++ include/pool.h
@@ -13,25 +13,17 @@
 
 typedef struct pool_mg_s pool_mg_s;
 typedef struct sub_pool_s sub_pool_s;
-// typedef struct w_node w_node;
 
 #define PAGEBLOCKS 64
 #define POOLSIZE (PAGESIZE * PAGEBLOCKS)
 #define SUBPOOLS 4
 #define SUBPOOLBLOCKS (PAGEBLOCKS / SUBPOOLS)
 
-#define PAGELOADACCP UINT32_MAX - 0
-#define PAGELOADFAIL UINT32_MAX - 1
-#define PAGESWAPACCP UINT32_MAX - 2
-#define PAGESWAPFAIL UINT32_MAX - 3
-#define PAGECANFREE UINT32_MAX - 4
-
-// struct w_node
-// {
-//     list_node_s link;
-//     pthread_t tid;
-    
-// };
+#define PAGELOADACCP 0
+#define PAGELOADFAIL 1
+#define PAGESWAPACCP 2
+#define PAGESWAPFAIL 3
+#define PAGECANFREE 4
 
 struct sub_pool_s
 {
@@ -51,24 +43,14 @@ struct pool_mg_s
 
     void *address_index_table[SUBPOOLS];
 };
-
-inline bool can_page_swap(block_s*);
+ 
 inline int get_block_spm_index(pool_mg_s*, block_s*);
-inline block_s* get_spm_block(sub_pool_s*);
-inline void free_spm_block(sub_pool_s*, block_s*);
 
-void block_priority_incr(pool_mg_s*, uint32_t);
-int pool_schedule(pool_mg_s*, disk_mg_s*, uint32_t);
 uint32_t page_swap_out(disk_mg_s*, block_s*);
 uint32_t page_bring_in(disk_mg_s*, uint32_t, block_s*);
 
-
 pool_mg_s* mp_pool_open(); 
 void mp_pool_close();
-
-// API use get single page, and free single page.
-block_s* mp_access_page_by_pid(pool_mg_s*, disk_mg_s*, uint32_t);
-void mp_access_page_finish(pool_mg_s*, disk_mg_s*, block_s*);
 
 bool mp_require_page_rlock(pool_mg_s*, block_s*);
 void mp_release_page_rlock(pool_mg_s*, block_s*);
--- src/pool/pool.c
+++ src/pool/pool.c
@@ -8,26 +8,6 @@
 
 #define FILELOCATION "pool.c"
 
-int get_block_spm_index(pool_mg_s *pm, block_s *block)
-{
-    uint64_t x = (uint64_t)block, y;
-    for (int i = 0; i < SUBPOOLS; i ++) {
-        y = (uint64_t)pm->address_index_table[i];
-        if (x > y) return i;
-    }
-    return -1;
-}
-
-bool can_page_swap(block_s *block)
-{
-    return (PINCHECK(block->flags)) ? true : false;
-}
-
-void block_priority_incr(pool_mg_s *pm, uint32_t pb_index)
-{
-
-}
-
 void* get_space_alignment_by(uint32_t value, uint32_t size)
 {
     char *buffer;
@@ -95,7 +75,24 @@ pool_mg_s* mp_pool_open()
     return pool;
 }
 
-// uncheck, may accessed by multi thread !!
+void mp_pool_close()
+{
+    
+}
+
+int get_block_spm_index(pool_mg_s *pm, block_s *block)
+{
+    uint64_t x = (uint64_t)block, y;
+    for (int i = 0; i < SUBPOOLS; i ++) {
+        y = (uint64_t)pm->address_index_table[i];
+        if (x > y) return i;
+    }
+    return -1;
+}
+
+/*-------------------------------------------------------------------*/
+
+
 uint32_t page_bring_in(disk_mg_s *dm, uint32_t page_id, block_s *block)
 {
     if (dk_read_page_by_pid(dm, page_id, (void*)block->page) == DKREADINCOMP)
@@ -103,7 +100,6 @@ uint32_t page_bring_in(disk_mg_s *dm, uint32_t page_id, block_s *block)
     return PAGELOADACCP;
 }
 
-// uncheck, may accessed by multi thread !!
 uint32_t page_swap_out(disk_mg_s *dm, block_s *block)
 {
     if (DIRTYCHECK(block->flags))
@@ -112,126 +108,7 @@ uint32_t page_swap_out(disk_mg_s *dm, block_s *block)
     return PAGESWAPACCP;
 }
 
-block_s* get_spm_block(sub_pool_s *spm)
-{
-    block_s *block;
-    if (!spm->empty_block) return NULL;
-    pthread_mutex_lock(&spm->sp_block_mutex);
-    block = spm->empty_block;
-    spm->empty_block = block->next_empty;
-    spm->used_blocks ++;
-    pthread_mutex_unlock(&spm->sp_block_mutex);
-    OCCUPYSET(&block->flags);
-    return block;
-}
 
-void free_spm_block(sub_pool_s *spm, block_s *block)
-{
-    pthread_mutex_lock(&(spm->sp_block_mutex));
-    block->next_empty = spm->empty_block;
-    spm->empty_block = block;
-    spm->used_blocks --;
-    pthread_mutex_unlock(&(spm->sp_block_mutex));
-    block->flags = 0;
-    block->priority = BLOCKPRIINIT;
-}
-
-block_s** allocate_blocks(pool_mg_s *pm, uint32_t needs) // didn't load page in memeory.
-{
-    block_s **blocks = (block_s**)malloc(sizeof(block_s*) * (needs + 1));
-    blocks[needs] = NULL;
-    int n_index = 0;
-
-    for (int i = 0; i < SUBPOOLS; i ++) {
-        for (int h = 0; h < SUBPOOLBLOCKS; h ++) {
-            if (SUBPOOLBLOCKS - pm->sub_pool[i].used_blocks >= needs) {
-                for (int j = 0; j < needs; j ++)
-                    blocks[n_index ++] = get_spm_block(&(pm->sub_pool[i]));
-                return blocks;
-            }
-        }
-    }
-    return NULL;
-}
-
-void free_blocks(pool_mg_s *pm, disk_mg_s *dm, block_s **blocks, bool need_free) // didn't write page in disk.
-{
-    for (int i = 0; blocks[i] != NULL; i ++) {
-        int spm_index = get_block_spm_index(pm, blocks[i]);
-        if (spm_index != -1) {
-            page_swap_out(dm, blocks[i]);
-            free_spm_block(&(pm->sub_pool[spm_index]), blocks[i]);
-        }
-    }
-    if (need_free) free(blocks);
-}
-
-// Find the block it can swap in disk, if not return NULL otherwise return sub-pool index.
-// Didn't write back page in disk !!
-int pool_schedule(pool_mg_s *pm, disk_mg_s *dm, uint32_t need_blocks)
-{
-    int n_index, spm_index;
-    block_s *needs[need_blocks + 1];
-    needs[need_blocks] = NULL;
-
-    for (int i = 0; i < SUBPOOLS && n_index < need_blocks; i ++) {
-        for (int h = 0; h < SUBPOOLBLOCKS && n_index < need_blocks; h ++)
-            if (can_page_swap(&(pm->sub_pool[i].buf_list[h])) && n_index < need_blocks)
-                needs[n_index ++] = &(pm->sub_pool[i].buf_list[h]);
-
-        if (n_index == need_blocks) {
-            spm_index = i;
-            break;
-        }
-        memset(needs, 0, n_index);
-        n_index = 0;
-    }
-    if (!n_index) return -1;
-
-    free_blocks(pm, dm, needs, false);
-
-    return spm_index;
-}
-
-block_s* mp_access_page_by_pid(pool_mg_s *pm, disk_mg_s *dm, uint32_t page_id)
-{
-    // check is it have different thread access the same page, if exist combine the access.
-    block_s **blocks = NULL;
-    block_s *block = NULL;
-    int spm_index;
-
-    for (int i = 0; i < SUBPOOLS; i ++) {
-        sub_pool_s *tmp = &pm->sub_pool[i];
-        pthread_mutex_lock(&tmp->sp_block_mutex);
-        for (int h = 0; h < SUBPOOLBLOCKS; h ++) {
-            if (OCCUPYCHECK(tmp->buf_list[h].flags) && tmp->buf_list[h].page->page_id == page_id) {
-                pthread_mutex_unlock(&tmp->sp_block_mutex);
-                return &(tmp->buf_list[h]);
-            }
-        }
-    }
-
-    if (!(blocks = allocate_blocks(pm, 1))) {
-        spm_index = pool_schedule(pm, dm, 1);
-        if (spm_index == -1)
-            return NULL;
-        else
-            blocks = allocate_blocks(pm, 1);
-    }
-    
-    block = blocks[0];
-    if (blocks) free(blocks);
-    page_bring_in(dm, page_id, block);
-    return block;
-}
-
-void mp_access_page_finish(pool_mg_s *pm, disk_mg_s *dm, block_s *block)
-{ 
-    block_s *blocks[2];
-    blocks[0] = block;
-    blocks[1] = NULL;
-    free_blocks(pm, dm, blocks, false);
-}
 
 bool mp_require_page_rlock(pool_mg_s *pm, block_s *block)
 {
