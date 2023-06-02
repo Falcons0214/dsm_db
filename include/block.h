16ae9f59b9e4fbd3eed06ebf940b594fca0da0c6
Falcons0214 <jeffery12366@yahoo.com>
2023-06-02 10:33:47 +0800

    Author: jojo Date: 2023/06/02 Title: Fix blcok.h flag set bug
    1. Move "&=" to "&".

--- include/block.h
+++ include/block.h
@@ -27,28 +27,28 @@ struct block_s
  */
 #define BPIN 0x00000001
 #define BDIRTY 0x00000002
-#define BREAD 0x00000003
-#define BWRITE 0x00000004
-#define BOCCUPY 0x00000005
+#define BREAD 0x00000004
+#define BWRITE 0x00000008
+#define BOCCUPY 0x00000010
 
 #define PINSET(f) ((*f) |= BPIN)
 #define PINCLEAR(f) ((*f) &= 0xfffffffe)
-#define PINCHECK(f) (f &= BPIN) ? true : false
+#define PINCHECK(f) (f & BPIN) ? true : false
 
 #define DIRTYSET(f) ((*f) |= BDIRTY)
 #define DIRTYCLEAR(f) ((*f) &= 0xfffffffd)
-#define DIRTYCHECK(f) (f &= BDIRTY) ? true : false
+#define DIRTYCHECK(f) (f & BDIRTY) ? true : false
 
 #define READSET(f) ((*f) |= BREAD)
-#define READCLEAR(f) ((*f) &= 0xfffffffc)
-#define READCHECK(f) (f &= BREAD) ? true : false
+#define READCLEAR(f) ((*f) &= 0xfffffffb)
+#define READCHECK(f) (f & BREAD) ? true : false
 
 #define WRITESET(f) ((*f) |= BWRITE)
-#define WRITECLEAR(f) ((*f) &= 0xfffffffb)
-#define WRITECHECK(f) (f &= BWRITE) ? true : false
+#define WRITECLEAR(f) ((*f) &= 0xfffffff8)
+#define WRITECHECK(f) (f & BWRITE) ? true : false
 
 #define OCCUPYSET(f) ((*f) |= BOCCUPY)
-#define OCCUPYCLEAR(f) ((*f) &= 0xfffffffa)
-#define OCCUPYCHECK(f) (f &= BOCCUPY) ? true : false
+#define OCCUPYCLEAR(f) ((*f) &= 0xffffffef)
+#define OCCUPYCHECK(f) (f & BOCCUPY) ? true : false
 
 #endif /* BLOCK_H */
